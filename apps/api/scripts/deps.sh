bfff7e7255ca398a2bc7bfe7a3fcf17689e0a517
killbasa <git@shaneb.xyz>
2024-12-29 13:13:33 -0500

    bump deps

--- apps/api/scripts/deps.sh
+++ apps/api/scripts/deps.sh
@@ -4,25 +4,35 @@
 ytdlp_url="https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp"
 ytdlp_path="./bin/yt-dlp"
 
+if [[ ! -e "./bin" ]]; then
+    mkdir "./bin"
+fi
+
 wget $ytdlp_url -O $ytdlp_path
 
 chmod +x $ytdlp_path
 echo "yt-dlp updated to $($ytdlp_path --version)"
 
+if [ "$(uname)" == "Darwin" ]; then
+	echo "FFMpeg builds not available for Mac OS X"
+	exit 0
+fi
+
 # ffmpeg
-ffmpeg_url="https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
-ffmpeg_path="./bin/ffmpeg"
 ffmpeg_dl_name="ffmpeg-master-latest-linux64-gpl"
+ffmpeg_url="https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/$ffmpeg_dl_name.tar.xz"
+ffmpeg_path="./bin/ffmpeg"
+ffprobe_path="./bin/ffprobe"
 
 wget $ffmpeg_url -O ./bin/$ffmpeg_dl_name.tar.xz
 
 tar -xf ./bin/$ffmpeg_dl_name.tar.xz -C ./bin
 
-mv ./bin/$ffmpeg_dl_name/bin/ffmpeg ./bin/ffmpeg
-mv ./bin/$ffmpeg_dl_name/bin/ffprobe ./bin/ffprobe
+mv ./bin/$ffmpeg_dl_name/bin/ffmpeg $ffmpeg_path
+mv ./bin/$ffmpeg_dl_name/bin/ffprobe $ffprobe_path
 
-chmod +x ./bin/ffmpeg
-chmod +x ./bin/ffprobe
+chmod +x $ffmpeg_path
+chmod +x $ffprobe_path
 
 rm -r ./bin/$ffmpeg_dl_name.tar.xz
 rm -r ./bin/$ffmpeg_dl_name
